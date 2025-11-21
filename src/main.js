02089bf7d088d1acb5951b48d9cdf614f3a8522e
Andrii <ahmedzanovandrej14@gmail.com>
2025-11-21 22:10:44 +0200

    new main-js

--- src/main.js
+++ src/main.js
@@ -6,9 +6,15 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import { useAuthStore } from '@/stores/auth'
+
 const app = createApp(App)
 
 app.use(createPinia())
+
+const authStore = useAuthStore()
+authStore.init()
+
 app.use(router)
 
 app.mount('#app')
